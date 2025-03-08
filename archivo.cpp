02eea89537a44996598b2a069c00ca30451adfaf
Piero Espada <espadita2510@gmail.com>
2025-03-08 17:40:02 -0500

    Subiendo el archivo de cpp

---
+++ archivo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string nombre = "Lucas";
+    string apellido = "Dalto";
+    string saludo = "Hola " + nombre + " " + apellido + ", como estas";
+    cout << saludo;
+}
